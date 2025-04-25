f96d846c1e8b802e773187ab64cbde7b00f6114d
Livan Andrés <andreslivancon@gmail.com>
2025-04-25 09:58:18 +0200

    Descripción de los cambios que hiciste

--- src/components/Profile/EditProfile.tsx
+++ src/components/Profile/EditProfile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAuth, User } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 
--- src/components/Profile/UserProfile.tsx
+++ src/components/Profile/UserProfile.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAuth, User } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import { Link, useParams } from 'react-router-dom'; // Import Link and useParams
 
